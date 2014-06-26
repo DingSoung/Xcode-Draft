da8334fe3949d7843803adbe578467ef2c1dc2b0
Ding Soung <DingSoung@gmail.com>
2014-06-26 22:33:38 +0800

    绘制方格和背景

--- MainViewController.swift
+++ MainViewController.swift
@@ -10,5 +10,45 @@ import UIKit
 
 class MainViewController:UIViewController
 {
+    //维度
+    var dimension:Int = 4
+    //过关值
+    var maxnumber:Int = 2048
+    //宽度
+    var width:CGFloat = 60
+    // 间距
+    var padding:CGFloat = 6
+    
+    //背景
+    var backgrounds:Array<UIView>
+    
+    init(){
+        self.backgrounds = Array<UIView>()
+        super.init(nibName:nil, bundle:nil)
+       
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //绘制背景和方格
+        setupBackground()
+    }
+    
+    func setupBackground(){
+        var x:CGFloat = 30
+        var y:CGFloat
+        
+        for i in 0..dimension{
+            y = 80
+            for j in 0..dimension{
+                var background = UIView(frame:CGRectMake(x,y,width,width))
+                background.backgroundColor = UIColor.darkGrayColor()
+                    self.view.addSubview(background)
+                backgrounds += background
+                y += padding + width
+            }
+            x += padding + width
+        }
+    }
     
 }
