d49f63a014bc9d35b228dccedff3d0afee596c68
Ding Soung <DingSoung@gmail.com>
2014-08-11 01:34:00 +0800

    Slider改动label,浮点百分数输出

--- PushKiller/ViewController.swift
+++ PushKiller/ViewController.swift
@@ -22,13 +22,13 @@ class ViewController: UIViewController {
     }
     
     
+    //    将控添加到storyboard，拉操作，拖到这里，就会有UIPickerView, 进去后会看到一些函数是用来配置的
+    @IBOutlet weak var timePicker: UIPickerView!
     let timeZone = ["码农，半夜别吵我",
         "上班族，早九晚五",
         "小学生作息",
         "中学生，早晚要自习",
         "大学生，随意睡懒觉"]
-    //    将控添加到storyboard，拉操作，拖到这里，就会有UIPickerView, 进去后会看到一些函数是用来配置的
-    @IBOutlet weak var timePicker: UIPickerView!
     //    列数
     func numberOfComponentsInPickerView(pickerView: UIPickerView!) -> Int {
         return 1
@@ -55,19 +55,24 @@ class ViewController: UIViewController {
     }
     
     
-    @IBOutlet weak var messageSlider: UISlider!
     @IBOutlet weak var messageLable: UILabel!
-    let messageFilter = ["10以内，不屏蔽",
-        "20以内,屏蔽明显的广告",
-        "30,屏蔽推广信息，推荐",
-        "40,蔽应用更新",
+    @IBOutlet weak var filterPersent: UILabel!
+    @IBOutlet weak var messageSlider: UISlider!
+    let messageFilter = ["不屏蔽",
+        "屏蔽明显的广告",
+        "屏蔽推广信息，推荐",
+        "蔽应用更新",
         "屏蔽所有非IM的通知，订阅",
         "屏蔽IM类的非联系人信息，社交，空间等",
         "屏蔽IM类公众信息，如群",
         "屏蔽IM信息",
         "屏蔽邮箱等非紧急信息",
         "屏蔽所有推送消息，短信，语音信箱，除了电话"]
-    //messageLable.text = messageFilter[(Int)(messageSlider.value * 10)]
+    @IBAction func messageFilter(sender: AnyObject) {
+        messageLable.text = messageFilter[(Int)(messageSlider.value * (Float)(messageFilter.count - 1))]
+    var str = NSString(format:"%2.1f",(messageSlider.value * 100))
+        filterPersent.text = "\(str)%"
+    }
     
     
     
