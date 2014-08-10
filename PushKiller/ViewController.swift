eb64cf23e1093d867b32471d7aa4a1a8bb7cc07a
Ding Soung <DingSoung@gmail.com>
2014-08-10 19:55:53 +0800

    picker 配置可以返回不同内容

--- PushKiller/ViewController.swift
+++ PushKiller/ViewController.swift
@@ -10,9 +10,6 @@ import UIKit
 import SpriteKit
 
 class ViewController: UIViewController {
-    var msgtime:Int8 = 0
-    var msgfilter:Int8 = 50
-    var msgstack:NSString = "later"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,61 +21,57 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func quiet(sender: UISlider) {
-        //     以小时为单位  0 ~ 2^8 小时
-        msgfilter = (Int8)(sender.value * 100)
-        msgfilter = (msgfilter/10) * 10
-    }
     
-    @IBOutlet weak var timePicker: UIPickerView!
+    let timeZone = ["码农，半夜别吵我",
+        "上班族，早九晚五",
+        "小学生作息",
+        "中学生，早晚要自习",
+        "大学生，随意睡懒觉"]
     //    将控添加到storyboard，拉操作，拖到这里，就会有UIPickerView, 进去后会看到一些函数是用来配置的
-    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var timePicker: UIPickerView!
     //    列数
     func numberOfComponentsInPickerView(pickerView: UIPickerView!) -> Int {
-        return 2
+        return 1
     }
     //    行数
     func pickerView(pickerView: UIPickerView!, numberOfRowsInComponent component: Int) -> Int {
-        return 10
+        //        不能用sizeof()
+        return timeZone.count
     }
-    //    显示的类容
+    //    每一行显示的类容
     func pickerView(pickerView: UIPickerView!, titleForRow row: Int, forComponent component: Int) -> String {
-        return "第\(row)行"
+        return timeZone[row];
+    }
+//    控制宽度高度
+    func pickerView(pickerView: UIPickerView!, widthForComponent component: Int) -> CGFloat {
+        return 100
+    }
+    func pickerView(pickerView: UIPickerView!, rowHeightForComponent component: Int) -> CGFloat {
+        return 21
+    }
+    func pickerView(pickerView: UIPickerView!, didSelectRow row: Int, inComponent component: Int) {
+//        选择后执行
+        var i = 0
     }
     
     
-    func configtime() {
-        
-    }
+    @IBOutlet weak var messageSlider: UISlider!
+    @IBOutlet weak var messageLable: UILabel!
+    let messageFilter = ["10以内，不屏蔽",
+        "20以内,屏蔽明显的广告",
+        "30,屏蔽推广信息，推荐",
+        "40,蔽应用更新",
+        "屏蔽所有非IM的通知，订阅",
+        "屏蔽IM类的非联系人信息，社交，空间等",
+        "屏蔽IM类公众信息，如群",
+        "屏蔽IM信息",
+        "屏蔽邮箱等非紧急信息",
+        "屏蔽所有推送消息，短信，语音信箱，除了电话"]
+    //messageLable.text = messageFilter[(Int)(messageSlider.value * 10)]
     
-    func configfilter() {
-        switch msgfilter {
-        case 0:
-            let filterdetail = "10以内，不屏蔽"
-        case 10:
-            let filterdetail = "20以内,屏蔽明显的广告"
-        case 20:
-            let filterdetail = "30,屏蔽推广信息，推荐"
-        case 30:
-            let filterdetail = "40,蔽应用更新"
-        case 40:
-            let filterdetail = "屏蔽所有非IM的通知，订阅"
-        case 50:
-            let filterdetail = "屏蔽IM类的非联系人信息，社交，空间等"
-        case 60:
-            let filterdetail = "屏蔽IM类公众信息，如群"
-        case 70:
-            let filterdetail = "屏蔽IM信息"
-        case 80:
-            let filterdetail = "屏蔽邮箱等非紧急信息"
-        case 90:
-            let filterdetail = "屏蔽所有推送消息，短信，语音信箱，除了电话"
-        default:
-            msgfilter = 50
-        }
-        
-    }
     
+    
+    var msgstack:NSString = "later"
     func configmsgstack() {
         switch msgstack {
         case "remind":
