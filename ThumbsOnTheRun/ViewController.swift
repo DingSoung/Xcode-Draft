6548b577e1c39640e156d0036f331afdff2a3d44
Ding Soung <DingSoung@gmail.com>
2014-08-28 00:18:20 +0800

    Navigation

---
+++ ThumbsOnTheRun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThumbsOnTheRun
+//
+//  Created by 丁松 on 14-8-27.
+//  Copyright (c) 2014年 丁松. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
