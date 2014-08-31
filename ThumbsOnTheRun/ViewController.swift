8d357b15252a802ab54c84f173953f6ed2e80221
Ding Soung <DingSoung@gmail.com>
2014-08-31 23:43:17 +0800

    集成PTMorphingLabel

--- ThumbsOnTheRun/ViewController.swift
+++ ThumbsOnTheRun/ViewController.swift
@@ -8,18 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, LTMorphingLabelDelegate {
+    
+    var i = 0
+    var textArray = ["Design", "Design is not just", "what it looks like", "and feels like.", "Design", "is how it works.", "- Steve Jobs", "Older people", "sit down and ask,", "'What is it?'", "but the boy asks,", "'What can I do with it?'.", "- Steve Jobs", "", "Swift", "Objective-C", "iPhone", "iPad", "Mac Mini", "MacBook Pro", "Mac Pro", "老婆 & 女儿"]
+    var text:String {
+        get {
+            if i >= countElements(textArray) {
+                i = 0
+            }
+            return textArray[i++]
+        }
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.welcomeLabel.delegate = self
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBOutlet weak var welcomeLabel: LTMorphingLabel!
+    @IBAction func welcomeButtom(sender: AnyObject) {
+        welcomeLabel.text = text
+    }
+    
+    @IBAction func welcomeStyle(sender: AnyObject) {
+        let seg = sender as UISegmentedControl
+        switch seg.selectedSegmentIndex {
+        case 1:
+            self.welcomeLabel.morphingEffect = .Evaporate
+        case 2:
+            self.welcomeLabel.morphingEffect = .Fall
+        case 3:
+            self.welcomeLabel.morphingEffect = .Pixelate
+        case 4:
+            self.welcomeLabel.morphingEffect = .Sparkle
+        case 5:
+            self.welcomeLabel.morphingEffect = .Burn
+        case 6:
+            self.welcomeLabel.morphingEffect = .Anvil
+        default:
+            self.welcomeLabel.morphingEffect = .Scale
+        }
+    }
+    
+    
+    
+    
+    
+    
 }
 
