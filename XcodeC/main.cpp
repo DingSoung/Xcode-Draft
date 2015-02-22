11fdd831f9f607d753ebb58ded26b9a68839b3fc
Ding Soung <DingSoung@gmail.com>
2015-02-22 20:20:02 +0800

    Initial Commit

---
+++ XcodeC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  XcodeC
+//
+//  Created by 丁松 on 15/2/22.
+//  Copyright (c) 2015年 丁松. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
