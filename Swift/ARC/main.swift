fd5ae64d58d12d9e134702c1ee1dada95fa1310e
Alex D <Alex@Alex.local>
2015-07-28 10:31:18 +0800

    测试swift的ARC 自动内存管理 引用计数

---
+++ Swift/ARC/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  ARC
+//
+//  Created by Alex D. on 15/7/27.
+//
+//
+
+import Foundation
+
+//Swift 项目也可以有一个名为 main.swift 特殊的文件。在这个文件中，我们不需要定义作用域，而可以直接书写代码。这个文件中的代码将作为 main 函数来执行。
+
+println("Hello, World!")
+
+var testARC: () = TestARC()
