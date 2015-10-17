17c3f25dfa5225fe92d43bfde417e174fa9cee25
Alex <Alex@Macbook.local>
2015-10-17 18:51:26 +0800

    new section

---
+++ niuniu/_Bases/ApplicationManager.swift
@@ -0,0 +1,13 @@
+//
+//  ApplicationManager.swift
+//  niuniu
+//
+//  Created by Alex on 15/10/17.
+//  Copyright © 2015年 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ApplicationManager: NSObject {
+
+}
---
+++ niuniu/_General/Extension/NSObject+Tools.swift
@@ -0,0 +1,63 @@
+//
+//  NSObjec+Tools.swift
+//  summer
+//
+//  Created by Alex D. on 15/8/12.
+//  Copyright (c) 2015年 Alex D. All rights reserved.
+//
+
+extension NSObject {
+    
+    //MARK: 闭包实现 C中的代码块功能
+    func closure(
+        mark:String? = "",
+        run:Bool? = true,
+        block:()->Void,
+        complete:(()->Void)? = {()->Void in
+            print("执行完毕")
+        }) {
+        if run != true { return }
+        block()
+        complete?()
+    }
+    func closure(block:()->Void) {
+        self.closure(block: block, complete: nil)
+    }
+    
+    
+    //MARK: JSON转model
+    #if false
+    init(JSONString: String) {
+    super.init()
+    var error : NSError?
+    let JSONData = JSONString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+    let JSONDictionary = NSJSONSerialization.JSONObjectWithData(JSONData!, options: NSJSONReadingOptions.MutableContainers, error: &error) as! NSDictionary
+    
+    for (key, value) in JSONDictionary {
+    let keyName = key as! String
+    let keyValue: String = value as! String
+    if (self.respondsToSelector(NSSelectorFromString(keyName))) {
+				self.setValue(keyValue, forKey: keyName)
+    }
+    }
+    }
+    #endif
+    init(JSONDic : [String : AnyObject]) {
+        super.init()
+        self.setValuesForKeysWithDictionary(JSONDic)
+    }
+    class func modelWithArray(dictArray:[NSDictionary]) -> NSArray {
+        let array: NSMutableArray = []
+        for dic in dictArray {
+            let model = super.init()
+            model.setValuesForKeysWithDictionary(dic as! [String : AnyObject])
+            array.addObject(model)
+        }
+        return array
+    }
+    override func setValue(value: AnyObject!, forUndefinedKey key: String) {
+        super.setValue(value, key)
+        print("未定义的key:\(key) for value:\(value)", terminator: "")
+    }
+    
+}
