d3604c4a321a2f7c8650df3a03a6823c37d9f3dd
DingSongWen <DingSongWen@haizhi.com>
2016-01-26 10:27:58 +0800

    cm

---
+++ SizeMenu/Example/SwiftSideMenu/UserSettingTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserSettingTableViewController.h
+//  SwiftSideMenu
+//
+//  Created by Alex D. on 16/1/22.
+//  Copyright © 2016年 Evgeny Nazarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserSettingTableViewController : UITableViewController
+
+@end
