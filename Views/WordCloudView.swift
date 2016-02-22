942e4d666720fbcf473589a159c83a82023b4944
DingSongWen <DingSongWen@haizhi.com>
2016-02-22 11:44:20 +0800

    coretext position problem

--- Views/WordCloudView.swift
+++ Views/WordCloudView.swift
@@ -53,13 +53,13 @@ class WordCloudView: UIView {
     override func drawRect(rect: CGRect) {
         super.drawRect(rect)
         
-        for _ in models {
+        for _ in self.models {
             //prepare word....
             let unitLenght:CGFloat = 20 //<-- min lenght of word
             var drawed = false
             
             //radius < view size * 0.5
-            for (var latitude = 0; drawed == false && CGFloat(latitude) * unitLenght < 0.5 * min(self.frame.size.width, self.frame.size.height); latitude++) {
+            for (var latitude = 0; drawed == false && CGFloat(latitude) * unitLenght < 0.5 * min(rect.size.width, rect.size.height); latitude++) {
                 
                 //2 * M_PI * radius = perimeter
                 //radian = unitLenght/radius = unitLenght / (latitude * unitLenght) = 1/latitude
@@ -76,20 +76,15 @@ class WordCloudView: UIView {
             }
         }
         
+        
         let context = UIGraphicsGetCurrentContext()
-        CGContextSetTextMatrix(context, CGAffineTransformIdentity)
         
-        // 移动坐标原点 默认坐标原点在屏幕左下角
-        CGContextTranslateCTM(context, 0 * self.frame.size.width, 1 * self.bounds.size.height)
-        
-        // 缩放比例  默认绘制方向的y轴正方向是向上的 改为向下
-        CGContextScaleCTM(context, 1, -1)
-        
-        // 旋转 相对于原点(0, 0) CGContextRotateCTM(context, 5 * M_PI / 180);
+        // 缩放比例  默认绘制方向的y轴正方向是向上的 改为向下->向下
+        CGContextScaleCTM(context, 1.0, -1.0)
         
         // 绘制通道 用于画text的区域
         let path = CGPathCreateMutable();
-        CGPathAddRect(path, nil, self.bounds); //足够大的区域
+        CGPathAddRect(path, nil, rect); //足够大的区域
         
         for var index = 0; index < self.models.count; index++ {
             // Config text font color
@@ -97,19 +92,21 @@ class WordCloudView: UIView {
             text.addAttribute(kCTForegroundColorAttributeName as String, value: self.models[index].color, range: NSMakeRange(0, text.length))
             text.addAttribute(kCTFontAttributeName as String, value: CTFontCreateWithName("Courier", 28 + CGFloat(index) * 5, nil), range: NSMakeRange(0, text.length))
             
-            //CGContextSetTextMatrix(context, CGAffineTransformMakeRotation(180 * CGFloat(M_PI) / 180.0)) //CGAffineTransformIdentity
-            
             // Backup.
             CGContextSaveGState(context)
             
+            
             // 配置坐标
-            //let suggestedSize = text.boundingRectWithSize(CGSizeMake(rect.size.width, 99999999), options: [NSStringDrawingOptions.UsesLineFragmentOrigin, NSStringDrawingOptions.UsesFontLeading], context: nil).size
-            //CGContextTranslateCTM(context, (self.frame.size.width - suggestedSize.width) * 0.5, (self.frame.size.height - suggestedSize.height) * 0.5)
-            //CGContextTranslateCTM(context, (self.frame.size.width) * 0.5, (self.frame.size.height) * 0.5)
+            CGContextTranslateCTM(context, 0 * rect.size.width, -1 * rect.size.height)
             
             // 配置旋转角度
             CGContextRotateCTM(context, -15 * CGFloat(index) * CGFloat(M_PI) / 180.0);
             
+            
+            
+            //let suggestedSize = text.boundingRectWithSize(CGSizeMake(rect.size.width, 99999999), options: [NSStringDrawingOptions.UsesLineFragmentOrigin, NSStringDrawingOptions.UsesFontLeading], context: nil).size
+            //CGContextTranslateCTM(context, (self.frame.size.width - suggestedSize.width) * 0.5, (self.frame.size.height - suggestedSize.height) * 0.5)
+            //CGContextTranslateCTM(context, (self.frame.size.width) * 0.5, (self.frame.size.height) * 0.5)
             
             //根据AttributedString生成CTFramesetterRef
             let frameSetter = CTFramesetterCreateWithAttributedString(text)
@@ -121,6 +118,54 @@ class WordCloudView: UIView {
             // Restore.
             CGContextRestoreGState(context);
         }
+        
+        
+        
+        /*
+        // Start drawing
+        UIGraphicsBeginImageContext(rect.size);
+        let context = UIGraphicsGetCurrentContext();
+        
+        // Retrieve the center and set a radius
+        
+        // Start by adjusting the context origin
+        // This affects all subsequent operations
+        CGContextTranslateCTM(context, rect.size.width * 0.5, rect.size.height * 0.5);
+        
+        // Iterate through the alphabet
+        for (var i = 0; i < self.models.count; i++) {
+            // Retrieve the letter and measure its display size
+            let letter = self.models[i].text as NSString
+            let letterSize = letter.sizeWithAttributes([NSFontAttributeName: UIFont.systemFontOfSize(14.0)])
+            
+            // Calculate the current angular offset
+            let theta = CGFloat(i) * (2 * CGFloat(M_PI) / CGFloat(self.models.count));
+            
+            // Encapsulate each stage of the drawing
+            CGContextSaveGState(context);
+            
+            
+            // Rotate the context
+            CGContextRotateCTM(context, theta);
+            
+            // Translate up to the edge of the radius and move left by
+            // half the letter width. The height translation is negative
+            // as this drawing sequence uses the UIKit coordinate system.
+            // Transformations that move up go to lower y values.
+            CGContextTranslateCTM(context, -letterSize.width / 2, -(min(rect.size.width * 0.5, rect.size.height * 0.5)));
+            
+            // Draw the letter and pop the transform state
+            letter.drawAtPoint(CGPointMake(0, 0), withAttributes: [NSFontAttributeName: UIFont.systemFontOfSize(14.0)])
+            
+            
+            CGContextRestoreGState(context);
+        }
+        
+        // Retrieve and return the image
+        let image = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        //return image;*/
     }
     
     private func checkSpace() -> Bool {
