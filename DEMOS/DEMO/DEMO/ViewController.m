544e7c84f60576110c25ce9ace8fc81936f2cb49
DingSongWen <DingSongWen@haizhi.com>
2016-03-18 14:22:59 +0800

    cmmmm

--- DEMOS/DEMO/DEMO/ViewController.m
+++ DEMOS/DEMO/DEMO/ViewController.m
@@ -18,8 +18,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    CCGaugeChartView *v1 = [[CCGaugeChartView alloc] init];
-    CCDescartesView *v2 = [[CCDescartesView alloc] init];
+    NSArray *imageArrayFromHtml = @[];
+    CGSize size;
+    if (imageArrayFromHtml.count >= 3) {
+        size = CGSizeMake(193, 128);
+    } else if (imageArrayFromHtml.count == 2) {
+        size = CGSizeMake(450, 438);
+    } else {
+        size = CGSizeMake(930, 480);
+    }
+    
+    NSMutableArray *outArray = [[NSMutableArray alloc] init];
+    for (NSInteger index = 0; index < imageArrayFromHtml.count && index < 3; index++) {
+        UIImageView *imgView = [[UIImageView alloc] initWithImage:imageArrayFromHtml[0]];
+        [imgView sizeToFit];
+        
+        UIImage *inImage = imageArrayFromHtml[index];
+        UIImage *outImage;
+        
+        if (imgView.bounds.size.width > size.width && imgView.bounds.size.height > size.height) {
+            [self resizeImageWithStream:inImage outImage:outImage width:size.width height:size.height];
+        } else {
+            [self resizeImageWithStream:inImage outImage:outImage width:192 height:128];
+        }
+        [outArray addObject:outImage];
+    }
+}
+
+- (void)resizeImageWithStream:(UIImage *)inImage outImage:(UIImage *)outImage width:(CGFloat)width height:(CGFloat)height {
+    //.....code here
 }
 
 - (void)didReceiveMemoryWarning {
