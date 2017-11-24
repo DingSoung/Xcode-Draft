9bab52d38643c193fabb38159e6946dd43f177bf
Songwen Ding <dingsongwen@bytedance.com>
2017-11-24 21:28:43 +0800

    精简

--- WKWebJS/WKWebJS/ViewController.swift
+++ WKWebJS/WKWebJS/ViewController.swift
@@ -17,66 +17,23 @@ class ViewController: BaseWebViewController {
         self.navigationItem.leftBarButtonItem?.title = "返回"
         
         self.webView.configuration.userContentController = WKUserContentController()
-        self.webView.uiDelegate = self
         
-         self.webView.configuration.userContentController.add(self, name: "Pay")
-         guard let  urlStr = Bundle.main.path(forResource: "index", ofType: "html") else { return }
-         let url = URL(fileURLWithPath: urlStr)
-         self.webView.loadFileURL(url, allowingReadAccessTo: url)
-        
+        self.webView.configuration.userContentController.add(self, name: "Pay")
+        guard let  urlStr = Bundle.main.path(forResource: "index", ofType: "html") else { return }
+        let url = URL(fileURLWithPath: urlStr)
+        self.webView.loadFileURL(url, allowingReadAccessTo: url)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
-
-public extension URL {
-    public func setCookie(value: String, forName name: String) {
-        var cookies = HTTPCookieStorage.shared.cookies(for: self) ?? []
-        for index in 0..<cookies.count {
-            if cookies[index].name == name {
-                cookies.remove(at: index)
-                break;
-            }
-        }
-        guard let cookie = HTTPCookie(properties: [
-            .name: name,
-            .value: value,
-            .originURL: self,
-            .version: 0,
-            .domain: self.host ?? "",
-            .path: "/",
-            .secure: self.scheme == "https://",
-            .expires: Date.init(timeIntervalSinceNow: 3 * 365 * 24 * 60 * 60),
-            .comment: "custom cookie",
-            //.commentURL:
-            //.discard
-            //.maximumAge
-            //.port
-            ]) else { return }
-        cookies.append(cookie)
-        HTTPCookieStorage.shared.setCookies(cookies, for: self, mainDocumentURL: nil)
-    }
-}
-
-fileprivate class DocsBarButtonItem: UIBarButtonItem {
-    var id = ""
-    var callback = ""
-}
-
-extension ViewController: WKScriptMessageHandler, WKUIDelegate  {
+extension ViewController: WKScriptMessageHandler  {
     // js call swift
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         print(message.name, message.body)
-    }
-    
-    @objc private func rightItensAction(sender: DocsBarButtonItem) {
-        self.webView.evaluateJavaScript(sender.callback, completionHandler: nil)
     }
 }
 
